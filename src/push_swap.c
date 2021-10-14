6c551993357e083f1442da4d797ce6e727e5d330
Plachido <placidopellegriti@live.it>
2021-10-14 17:53:30 +0000

    All done, norm missing

--- src/push_swap.c
+++ src/push_swap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: plpelleg <plpelleg@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 16:25:30 by plpelleg          #+#    #+#             */
-/*   Updated: 2021/10/14 15:52:39 by plpelleg         ###   ########.fr       */
+/*   Updated: 2021/10/14 17:53:00 by ubuntu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	main(int argc, char **argv)
 		a = ft_char_to_stack(argc, argv);
 		ft_duplicates(a);
 		b = NULL;
-		if (argc <= 6)
+		if (argc <= 25)
 			a = ft_sort_small(a, b, argc-1);
 		a = ft_sort(a, b, argc-1);
 		ft_free(a, b);
